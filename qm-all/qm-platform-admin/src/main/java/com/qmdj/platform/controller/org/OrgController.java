d3d91056b32b0a58b14a1299b512976524b71f3b
木头人 <994607009@qq.com>
2016-10-13 13:14:42 +0800

    首页改造

--- qm-all/qm-platform-admin/src/main/java/com/qmdj/platform/controller/org/OrgController.java
+++ qm-all/qm-platform-admin/src/main/java/com/qmdj/platform/controller/org/OrgController.java
@@ -1,6 +1,27 @@
 package com.qmdj.platform.controller.org;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/org")
 public class OrgController {
 
-	
+	@RequestMapping("/add")
+   public  String  toAddOrg(Model model,HttpServletRequest request,HttpServletResponse response){
+	   return "org/add.html";
+   }
+   
+	@RequestMapping("/list")
+	   public  String  orgList(Model model,HttpServletRequest request,HttpServletResponse response){
+		   System.out.println("ssssssssssss");
+		   return "org/list.html";
+	   }
+	   
+   
+   
 }
